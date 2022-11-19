500832c492057642ccad8f28d673afdc19151460
Felipe P <fpalmasaldivia@gmail.com>
2022-11-18 21:05:56 -0300

    se crea primera tabla cn datos

--- Desafio3.sql
+++ Desafio3.sql
@@ -8,4 +8,11 @@ R: CREATE TABLE usuarios (id SERIAL, nombre VARCHAR, apellido VARCHAR, email VAR
 
 R: 
 INSERT INTO usuarios (nombre. apellido, email, rol) values ('Andrea', 'Palma', 'apalma@chile.cl', 'Administrador');
-INSERT INTO usuarios (nombre, apellido, email, rol) values
+INSERT INTO usuarios (nombre, apellido, email, rol) values ('Fredy', 'Palma' 'FredyP@google.com', 'usuario');
+INSERT INTO usuarios (nombre, apellido, email, rol) values ('Felipe'. 'Palma', 'Felipe.Palma@gmail.com' 'usuario');
+INSERT INTO usuarios (nombre, apellido, email, rol) values ('Sofia', 'Palma', 'SofiaP@hotmail.com', 'usuarios');
+INSERT INTO usuarios (nombrea,apellido, email, rol) values ('Maria', 'Saldivia' 'Mariasaldivia@google.cl' 'usuario');
+
+--se crea tabla post
+R: CREATE TABLE post(id SERIAL, titulo VARCHAR, contenido TEXT, fecha_creacion TIMESTAMP, fecha_actualizacion TIMESTAMP, destacado BOOLEAN, usuario_id BIGINT);
+
