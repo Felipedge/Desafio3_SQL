32a0a304db9de126c92a6019f64e283abb7a657e
Felipe P <fpalmasaldivia@gmail.com>
2022-11-19 19:16:03 -0300

    respuesta de preguntas parte 1

--- Desafio3.sql
+++ Desafio3.sql
@@ -7,22 +7,22 @@ R: CREATE TABLE usuarios (id SERIAL, nombre VARCHAR, apellido VARCHAR, email VAR
 -- se agregan datos de usuaroios a tabla "usuaroos"
 
 R: 
-INSERT INTO usuarios (nombre. apellido, email, rol) values ('Andrea', 'Navas', 'apalma@chile.cl', 'Administrador');
-INSERT INTO usuarios (nombre, apellido, email, rol) values ('Fredy', 'Rojas' 'FredyP@google.com', 'usuario');
-INSERT INTO usuarios (nombre, apellido, email, rol) values ('Felipe'. 'Palma', 'Felipe.Palma@gmail.com' 'usuario');
-INSERT INTO usuarios (nombre, apellido, email, rol) values ('Sofia', 'Perez', 'SofiaP@hotmail.com', 'usuarios');
-INSERT INTO usuarios (nombrea,apellido, email, rol) values ('Maria', 'Saldivia' 'Mariasaldivia@google.cl' 'usuario');
+INSERT INTO usuarios (id, nombre. apellido, email, rol) values (1, 'Andrea', 'Navas', 'apalma@chile.cl', 'Administrador');
+INSERT INTO usuarios (id, nombre, apellido, email, rol) values (2, 'Fredy', 'Rojas' 'FredyP@google.com', 'usuario');
+INSERT INTO usuarios (id, nombre, apellido, email, rol) values (3, 'Felipe'. 'Palma', 'Felipe.Palma@gmail.com' 'usuario');
+INSERT INTO usuarios (id, nombre, apellido, email, rol) values (4, 'Sofia', 'Perez', 'SofiaP@hotmail.com', 'usuarios');
+INSERT INTO usuarios (id, nombrea,apellido, email, rol) values (5, 'Maria', 'Saldivia' 'Mariasaldivia@google.cl' 'usuario');
 
 --se crea tabla post
 R: CREATE TABLE post(id SERIAL, titulo VARCHAR, contenido TEXT, fecha_creacion TIMESTAMP, fecha_actualizacion TIMESTAMP, destacado BOOLEAN, usuario_id BIGINT);
 
 --se  agregan datos a tabla "post"
 R:
-INSERT INTO post (titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values ('La comida chilena' 'La comida chilena actual posee una gran influencia de nuestros vecinos preuanos...', '12-12-1990', '02-03-1991', true, 1 );
-INSERT INTO post (titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values ('El futbol profesional chileno no es bueno', 'segun estadisticas extraidas desde la web de FIFA, se puede indicar que la liga profesional chilena no posee los requisitos para considerarse dentro de las mejores de America...', '21-03-2001', '04-10-2002' true, 1 );
-INSERT INTO post (titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values ('El rock no esta muerto', 'Segun diversas encuentas indican que la musica rock no esta muerta, pero si hay un gran numero de oyentes que no estan dispuesto a escuchar a bandas nuevas...' '23-01-2021', '01-10-2021', true,2);
-INSERT INTO post (titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values ('Las redes sociales son la nueva forma de interaccion', 'Segun estudios cada vez las interacciones entre humanos van disminuyendo debido al alto alcance de la tecnologia y las RRSS', '11-04-2022','08-05-2022', false,3);
-INSERT INTO post (titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values ('El formato de trabaji hibrido llego para quedarse', 'Con los acontecimientos ocurridos durante los aultimos años se han incrementado las formas de trabajo alternativas como lo es el trabajo de forma hibrida y por objetivos', '06-10-2021', '02-01-2022', false, null);
+INSERT INTO post (id, titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values (1, 'La comida chilena' 'La comida chilena actual posee una gran influencia de nuestros vecinos preuanos...', '12-12-1990', '02-03-1991', true, 1 );
+INSERT INTO post (id, titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values (2, 'El futbol profesional chileno no es bueno', 'segun estadisticas extraidas desde la web de FIFA, se puede indicar que la liga profesional chilena no posee los requisitos para considerarse dentro de las mejores de America...', '21-03-2001', '04-10-2002' true, 1 );
+INSERT INTO post (id, titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values (3, 'El rock no esta muerto', 'Segun diversas encuentas indican que la musica rock no esta muerta, pero si hay un gran numero de oyentes que no estan dispuesto a escuchar a bandas nuevas...' '23-01-2021', '01-10-2021', true,2);
+INSERT INTO post (id, titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values (4, 'Las redes sociales son la nueva forma de interaccion', 'Segun estudios cada vez las interacciones entre humanos van disminuyendo debido al alto alcance de la tecnologia y las RRSS', '11-04-2022','08-05-2022', false,3);
+INSERT INTO post (id, titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) values (5, 'El formato de trabaji hibrido llego para quedarse', 'Con los acontecimientos ocurridos durante los aultimos años se han incrementado las formas de trabajo alternativas como lo es el trabajo de forma hibrida y por objetivos', '06-10-2021', '02-01-2022', false, null);
 
 
 --se crea tabla comentarios
@@ -30,11 +30,11 @@ R: CREATE TABLE comentarios (id SERIAL, contenido TEXT, fecha_creacion TIMESTAMP
 
 --se agregan datots a tabla cpmentarios
 R:
-INSERT INTO comentarios (contenido, fecha_creacion, usuario_id, post_id) values ('Hola','12-12-1990', 1, 1,);
-INSERT INTO comentarios (contenido, fecha_creacion, usuario_id, post_id) values ('Pizza','21-03-2001', 2, 1,);
-INSERT INTO comentarios (contenido, fecha_creacion, usuario_id, post_id) values ('Chao','23-01-2021', 3, 1,);
-INSERT INTO comentarios (contenido, fecha_creacion, usuario_id, post_id) values ('Mensaje','11-04-2022', 1, 2,);
-INSERT INTO comentarios (contenido, fecha_creacion, usuario_id, post_id) values ('Bar','06-10-2021', 2, 2,);
+INSERT INTO comentarios (id, contenido, fecha_creacion, usuario_id, post_id) values (1, 'Hola','12-12-1990', 1, 1,);
+INSERT INTO comentarios (id, contenido, fecha_creacion, usuario_id, post_id) values (2, 'Pizza','21-03-2001', 2, 1,);
+INSERT INTO comentarios (id, contenido, fecha_creacion, usuario_id, post_id) values (3, 'Chao','23-01-2021', 3, 1,);
+INSERT INTO comentarios (id, contenido, fecha_creacion, usuario_id, post_id) values (4, 'Mensaje','11-04-2022', 1, 2,);
+INSERT INTO comentarios (id, contenido, fecha_creacion, usuario_id, post_id) values (5, 'Bar','06-10-2021', 2, 2,);
 
 --PREGUNTAS!!!!
 
@@ -44,4 +44,6 @@ SELECT user.nombre, user.email, posteos.titulo, posteos.contenido FROM usuarios 
 
 
 --Muestra el id, título y contenido de los posts de los administradores. El administrador puede ser cualquier id y debe ser seleccionado dinámicamente.(1 Punto).
+
+SELECT
 
